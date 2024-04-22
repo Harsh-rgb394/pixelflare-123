e608d8994ebfe3d77733475563cecd7935208441
Harshvardhansingh <harshitharsha778@gmail.com>
2024-04-22 16:19:40 +0530

    changemade

--- server/routes/AIRoutes.js
+++ server/routes/AIRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -20,14 +20,14 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const Response = await openai.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const image = Response.data.data[0].b64_json;
     res.status(200).json({ success:true,photo: image });
   } catch (error) {
     console.error(error);
